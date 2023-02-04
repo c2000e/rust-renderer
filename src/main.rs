7d47081571eacfe888ed77e487b972ac7a919650
Christian Ermann <christianermann@gmail.com>
2023-02-03 19:57:31 -0500

    Open a window.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+use winit::{
+    event::{Event, WindowEvent},
+    event_loop::EventLoop,
+    window::Window,
+};
+
 fn main() {
-    println!("Hello, world!");
+    let event_loop = EventLoop::new();
+    let window = Window::new(&event_loop).unwrap();
+    window.set_title("rust-renderer");
+
+    event_loop.run(move |event, _, control_flow| {
+        control_flow.set_poll();
+
+        match event {
+            Event::WindowEvent {
+                event: WindowEvent::CloseRequested,
+                ..
+            } => control_flow.set_exit(),
+            _ => (),
+        }
+    });
 }
